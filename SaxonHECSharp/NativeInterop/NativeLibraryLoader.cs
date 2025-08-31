cc9e4639f480a637fc35c8b5b7fb5e82b0a1740e
Hieu Dam <dhhieu113@gmail.com>
2025-08-31 13:36:11 +0700

    Added missing using directives for List<> and Linq

--- SaxonHECSharp/NativeInterop/NativeLibraryLoader.cs
+++ SaxonHECSharp/NativeInterop/NativeLibraryLoader.cs
@@ -2,6 +2,8 @@ using System;
 using System.IO;
 using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SaxonHECSharp.NativeInterop
 {
