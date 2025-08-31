5f430d8382a0a09d8057c41805d4fe36ba21db03
Hieu Dam <dhhieu113@gmail.com>
2025-08-31 19:56:30 +0700

    fix name

--- SaxonHECSharp/NativeInterop/SaxonNative.cs
+++ SaxonHECSharp/NativeInterop/SaxonNative.cs
@@ -147,8 +147,8 @@ namespace SaxonHECSharp.NativeInterop
             }
             else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
             {
-                string versioned = Path.Combine(nativeDir, $"lib{libraryName}.12.8.0.dylib");
-                string soname = Path.Combine(nativeDir, $"lib{libraryName}.12.dylib");
+                string versioned = Path.Combine(nativeDir, $"lib{libraryName}.so.12.8.0");
+                string soname = Path.Combine(nativeDir, $"lib{libraryName}.so.12");
                 try
                 {
                     var ln = new System.Diagnostics.ProcessStartInfo
@@ -189,8 +189,8 @@ namespace SaxonHECSharp.NativeInterop
             }
             else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
             {
-                string versioned = Path.Combine(nativeDir, $"lib{libraryName}.dylib.12.8.0");
-                string soname = Path.Combine(nativeDir, $"lib{libraryName}.dylib.12");
+                string versioned = Path.Combine(nativeDir, $"lib{libraryName}.12.8.0.dylib");
+                string soname = Path.Combine(nativeDir, $"lib{libraryName}.12.dylib");
                 try
                 {
                     var ln = new System.Diagnostics.ProcessStartInfo
